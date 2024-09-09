4490eb3ea1f119a7ab91fc8eb509f5587e654698
Chen Asraf <casraf@pm.me>
2024-09-10 01:44:48 +0300

    feat: dynamic help keys

--- ui/list_model.go
+++ ui/list_model.go
@@ -54,13 +54,15 @@ const (
 var debug io.Writer
 
 type ListCtrl[T ListModel[C], C comparable] struct {
-	question      string
-	width, height int
-	cursor        int
-	list          T
-	exitState     ExitState
-	filter        *ListFilter[C]
-	items         []*Choice[C]
+	question       string
+	width, height  int
+	cursor         int
+	list           T
+	exitState      ExitState
+	filter         *ListFilter[C]
+	items          []*Choice[C]
+	listHelpKeys   []HelpEntry
+	filterHelpKeys []HelpEntry
 	// TODO help delegates
 }
 
@@ -84,10 +86,27 @@ func NewListCtrl[T ListModel[C], C comparable](list T) *ListCtrl[T, C] {
 	}
 	debug = dump
 	filter := &ListFilter[C]{}
+	listHelpKeys := []HelpEntry{
+		NewHelpEntry("j/k/up/down", "move"),
+		NewHelpEntry("space", "toggle"),
+		NewHelpEntry("enter", "confirm"),
+		NewHelpEntry("/", "search"),
+		// NewHelpEntry("a", "select all"),
+		// NewHelpEntry("c", "clear selection"),
+	}
+	filterHelpKeys := []HelpEntry{
+		NewHelpEntry("esc", "clear filter"),
+		NewHelpEntry("enter", "confirm"),
+	}
+
 	return &ListCtrl[T, C]{
-		list:   list,
-		filter: filter,
-		items:  list.Items(),
+		width:          80,
+		height:         40,
+		list:           list,
+		filter:         filter,
+		items:          list.Items(),
+		listHelpKeys:   listHelpKeys,
+		filterHelpKeys: filterHelpKeys,
 	}
 }
 
@@ -260,18 +279,110 @@ func (m *ListCtrl[T, C]) RenderStatusBar() (string, int) {
 	return s.String(), c
 }
 
+type HelpEntry struct {
+	key, action string
+}
+
+func NewHelpEntry(key, action string) HelpEntry {
+	return HelpEntry{key, action}
+}
+
+func renderHelpKey(entry HelpEntry) (string, int) {
+	var (
+		tok Colored
+		c   int
+	)
+	tok = NewColored(ColorWhite, entry.key)
+	keyStr := tok.String()
+	c += tok.TextLength()
+
+	tok = NewColored(ColorDim, entry.action)
+	actionStr := tok.String()
+	c += tok.TextLength()
+	c += 2
+	return fmt.Sprintf("%s  %s", keyStr, actionStr), c
+}
+
+func (m *ListCtrl[T, C]) GetHelpColumnCount() int {
+	max := 0
+	for _, key := range m.ActiveHelpKeys() {
+		if _, cc := renderHelpKey(key); cc > max {
+			max = cc
+		}
+	}
+	width := m.width
+	if m.list.Style().DrawBorder {
+		width -= 2
+	}
+	// NOTE 2 spaces min between items
+	max += 2
+
+	fit := width / max
+	spew.Fprintf(debug, "[ColumnCount] width: %d, max: %d, fit: %d\n", width, max, fit)
+	return utils.MaxInt(fit, 1)
+}
+
+func (m *ListCtrl[T, C]) GetHelpRowCount() int {
+	fit := m.GetHelpColumnCount()
+	return int(math.Ceil(float64(len(m.listHelpKeys)) / float64(fit)))
+}
+
 func (m *ListCtrl[T, C]) RenderFooter() string {
 	border := m.list.Style().DrawBorder
 	width := m.width
 	var s strings.Builder
 
-	help := "j/k/up/down - move cursor      space - toggle      enter - done"
+	var (
+		lines   []string
+		lengths []int
+	)
+	c := 0
+	line := ""
+	fit := m.GetHelpColumnCount()
+	spew.Fprintf(debug, "fit: %d, width: %d\n", fit, width)
 
-	tok := NewColored(ColorDim, help)
 	s.WriteString(wrapWithBorder(border, "", 0, width))
-	s.WriteString(wrapWithBorder(border, tok.String(), tok.TextLength(), width))
+
+	for i, key := range m.ActiveHelpKeys() {
+		str, cc := renderHelpKey(key)
+
+		nextWillOverflow := (i+1)%fit == 0
+
+		if !nextWillOverflow {
+			spacing := width/fit - cc
+			spew.Fprintf(debug, "width: %d, fit: %d, c: %d, spacing: %d\n", width, fit, cc, spacing)
+			sep := strings.Repeat(" ", utils.MaxInt(0, spacing))
+			str += sep
+			line += str
+			c += cc + len(sep)
+			spew.Fprintf(debug, "adding line: %s, c: %d, len: %d\n", str, c, len(line))
+		} else {
+			line += str
+			c += cc
+			lines = append(lines, line)
+			lengths = append(lengths, c)
+			line = ""
+			c = 0
+		}
+	}
+
+	if len(line) > 0 {
+		lines = append(lines, line)
+		lengths = append(lengths, c)
+	}
+	for i, line := range lines {
+		spew.Fprintf(debug, "printing line: %s, len: %d\n", line, lengths[i])
+		s.WriteString(wrapWithBorder(border, line, lengths[i], width))
+	}
 
 	return s.String()
+}
+
+func (m *ListCtrl[T, C]) ActiveHelpKeys() []HelpEntry {
+	if m.filter.mode == FilterFocused {
+		return m.filterHelpKeys
+	}
+	return m.listHelpKeys
 }
 
 func wrapWithBorder(border bool, s string, size int, width int) string {
@@ -334,9 +445,9 @@ func (m *ListCtrl[T, C]) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 			return m, m.MoveCursor(-1)
 		case "down", "j":
 			return m, m.MoveCursor(1)
-		case "left", "pgup":
+		case "left", "pgup", "h":
 			return m, m.MoveCursor(-10)
-		case "right", "pgdown":
+		case "right", "pgdown", "l":
 			return m, m.MoveCursor(10)
 		case " ":
 			choice := m.items[m.cursor]
@@ -426,10 +537,13 @@ func (m *ListCtrl[T, C]) HeaderHeight() int {
 }
 
 func (m *ListCtrl[T, C]) FooterHeight() int {
-	h := 2
-	if !m.list.Style().HelpEnabled {
-		h--
+	rowCount := m.GetHelpRowCount()
+
+	h := 0
+	if m.list.Style().HelpEnabled {
+		h += rowCount + 1
 	}
+
 	return h
 }
 
